6a99cba74dd1aae135b4a3f9d6be8789866c57d0
aleksandra-lisek <olalisek.al@gmail.com>
2019-08-02 18:15:36 +0200

    airbnb eslint added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import classes from './App.module.css';
 
-function App() {
-  return (
-    <div className={classes.App}>
-      <header className={classes.AppHeader}>
-        <h1>appka</h1>
-      </header>
-    </div>
-  );
-}
+const app = () => (
+  <div className={classes.App}>
+    <header className={classes.AppHeader}>
+      <h1>appka</h1>
+    </header>
+  </div>
+);
 
-export default App;
+
+export default app;
