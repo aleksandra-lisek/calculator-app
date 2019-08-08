41f3b4066580a264ebbb5b09f7566efd53535906
aleksandra-lisek <olalisek.al@gmail.com>
2019-08-08 21:36:07 +0200

    Updates

---
+++ precache-manifest.cbc9ddc786dfe5df05f07f50a0c05ce2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80a409fefa02cc91ee478bff5f9f7b0",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "24fcddbdbae59f3ca736",
+    "url": "/repository-name/static/css/main.a7320003.chunk.css"
+  },
+  {
+    "revision": "e64648626697fa61c9a0",
+    "url": "/repository-name/static/js/2.ab3ca68f.chunk.js"
+  },
+  {
+    "revision": "24fcddbdbae59f3ca736",
+    "url": "/repository-name/static/js/main.8fe7e685.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "60558de64b5f861e82753bd4ed809c33",
+    "url": "/repository-name/static/media/SamsungSharpSans-Bold.60558de6.woff"
+  },
+  {
+    "revision": "ccf719dec200ac16f50d9ca11c33217c",
+    "url": "/repository-name/static/media/SamsungSharpSans-Medium.ccf719de.woff"
+  },
+  {
+    "revision": "0fabfcc1433dff3a45c9a3f4dee5a7d9",
+    "url": "/repository-name/static/media/SamsungSharpSans-Regular.0fabfcc1.woff"
+  },
+  {
+    "revision": "527889780413151943a5937ca4ae26ff",
+    "url": "/repository-name/static/media/calc_desktop.52788978.png"
+  }
+]);
